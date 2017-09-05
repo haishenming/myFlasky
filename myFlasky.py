5b9654a621e94ce3855e83bde7e670326e4779ca
xiangyufan <xiangyufan@juwang.cn>
2017-09-05 16:13:14 +0800

    第三章

--- myFlasky.py
+++ myFlasky.py
@@ -1,23 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask import make_response
 from flask import redirect
+
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
-    response = make_response("<h1>make response</h1>")
-    response.set_cookie('answer', '1')
-    return response
+def index():
+    return render_template("index.html")
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello {name}!<h1>'.format(name=name)
+    return render_template("user.html", name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error_page/404.html', error=e), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error_page/500.html', error=e), 500
+
 
 
 if __name__ == '__main__':
