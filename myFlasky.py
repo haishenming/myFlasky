8fcdc59ef55dd11ee80ab5e2e05a2049177ed3b8
xiangyufan <xiangyufan@juwang.cn>
2017-09-06 15:43:21 +0800

    flash

--- myFlasky.py
+++ myFlasky.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, session, url_for
+from flask import Flask, render_template, session, url_for, flash
 from flask import request
 from flask import make_response
 from flask import redirect
@@ -8,7 +8,7 @@ from flask import redirect
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -20,7 +20,7 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
@@ -28,6 +28,9 @@ class NameForm(Form):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template("index.html",
