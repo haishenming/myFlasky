cf9c89a27552e707d6bd2e9afd29612ef5245b0d
xiangyufan <xiangyufan@juwang.cn>
2017-09-06 15:07:07 +0800

    表单提交

--- myFlasky.py
+++ myFlasky.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for
 from flask import request
 from flask import make_response
 from flask import redirect
@@ -10,6 +10,9 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
 
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
@@ -17,10 +20,20 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html",
+                           current_time=datetime.utcnow(),
+                           form=form,
+                           name=session.get('name'))
 
 
 @app.route('/user/<name>')
