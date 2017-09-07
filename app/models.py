4d6645863d1f5510b1080159ba977c7ebd493545
xiangyufan <xiangyufan@juwang.cn>
2017-09-07 15:24:06 +0800

    程序重构

---
+++ app/models.py
@@ -0,0 +1,25 @@
+"""
+数据库模型
+"""
+
+from . import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role {name}>'.format(name=self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User {name}>'.format(name=self.name)
---
+++ manage.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+import os
+
+from app import create_app, db
+from app.models import User, Role
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+# shell启动配置
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+@manager.command
+def test():
+    import unittest
+    test = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(test)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
