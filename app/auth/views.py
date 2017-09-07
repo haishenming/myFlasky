29a0808152ac04c6dc42377ddde6a96fd69adca1
xiangyufan <xiangyufan@juwang.cn>
2017-09-07 18:30:15 +0800

    logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, redirect, request, url_for, flash
 
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 from . import auth
 from ..models import User
@@ -17,3 +17,10 @@ def login():
             return redirect(request.args.get('next') or url_for('main_index'))
         flash("Invalid username or password.")
     return render_template('auth/login.html', form=form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.')
+    return redirect(url_for("main.index"))
