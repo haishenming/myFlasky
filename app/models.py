977dd67f728ddf17e396588b70b65d3ed3f2532d
xiangyufan <xiangyufan@juwang.cn>
2017-09-08 12:26:35 +0800

    upgrade table role

--- app/models.py
+++ app/models.py
@@ -11,8 +11,10 @@ from . import db, login_manager
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
+    name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.String(12))
 
     def __repr__(self):
         return '<Role {name}>'.format(name=self.name)
